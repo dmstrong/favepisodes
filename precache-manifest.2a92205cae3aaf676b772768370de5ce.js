72b7d8ca222c2f234cd3bec942207ad71c6f353a
dmstrong <dmstrong07@gmail.com>
2019-12-27 06:33:19 -0600

    Updates

---
+++ precache-manifest.2a92205cae3aaf676b772768370de5ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ac7275b24eca9a9099d9e31f9d687b",
+    "url": "/favepisodes/index.html"
+  },
+  {
+    "revision": "8049f5de2dc55226e622",
+    "url": "/favepisodes/static/css/main.9a1c4ddf.chunk.css"
+  },
+  {
+    "revision": "c91b23ac82e6fea7c579",
+    "url": "/favepisodes/static/js/0.6ba3d7fa.chunk.js"
+  },
+  {
+    "revision": "b8368b1788e31b61080f",
+    "url": "/favepisodes/static/js/3.4b3df0a7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/favepisodes/static/js/3.4b3df0a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8049f5de2dc55226e622",
+    "url": "/favepisodes/static/js/main.707a215d.chunk.js"
+  },
+  {
+    "revision": "aab7a28204a5b006f816",
+    "url": "/favepisodes/static/js/runtime-main.671bd249.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/favepisodes/precache-manifest.2c05274de582f81e9b06c824a4577763.js"
+  "/favepisodes/precache-manifest.2a92205cae3aaf676b772768370de5ce.js"
 );
 
 self.addEventListener('message', (event) => {
